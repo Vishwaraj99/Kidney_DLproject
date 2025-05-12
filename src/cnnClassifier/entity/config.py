511a9905387fe54a8c51fae67b2005204e6f0624
Vishwaraj99 <vishwarajpatil831@gmail.com>
2025-05-12 17:03:21 +0530

    model eval

--- src/cnnClassifier/entity/config.py
+++ src/cnnClassifier/entity/config.py
@@ -4,13 +4,10 @@ from pathlib import Path
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
-    root_dir: Path
-    source_URL: str
-    local_data_file: Path
-    unzip_dir: Path
-
-
-
+    root_dir : Path
+    source_URL : str
+    local_data_file : Path
+    unzip_dir : Path
 
 
 @dataclass(frozen=True)
@@ -24,8 +21,7 @@ class PrepareBaseModelConfig:
     params_weights: str
     params_classes: int
 
-from dataclasses import dataclass
-from pathlib import Path
+
 
 
 @dataclass(frozen=True)
@@ -38,3 +34,12 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
